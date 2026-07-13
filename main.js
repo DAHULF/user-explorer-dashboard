5241edb8f5bdc48c951b933c91cc76d5bf10761a
bebra <danil.dugin@bk.ru>
2026-07-13 12:12:43 +0300

    The design of the page for mobile devices is fully completed.

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+"use strict";
+
 lucide.createIcons();
+
+const navSidebar = document.querySelector(".nav-sidebar");
+const showNavSidebarBtn = document.querySelector(".show-nav-sidebar-icon");
+const hideNavSidebarBtn = document.querySelector(".hide-nav-sidebar-icon");
+
+//navSidebar.classList.toggle('nav-sidebar-displayed', true);
+
+showNavSidebarBtn.addEventListener("click", () => {
+    navSidebar.classList.toggle('nav-sidebar-displayed', true);
+});
+
+hideNavSidebarBtn.addEventListener("click", () => {
+    navSidebar.classList.toggle('nav-sidebar-displayed', false);
+});
